abbb3f9a7c039183bd21165c93479cd1852ed48a
ShulhaOleh <broo20544@gmail.com>
2026-05-13 18:20:05 -0400

    chore(dev): auto-open DevTools in development mode

--- src/main/index.ts
+++ src/main/index.ts
@@ -64,6 +64,7 @@ function createWindow(): void {
 
     if (process.env['ELECTRON_RENDERER_URL']) {
         win.loadURL(process.env['ELECTRON_RENDERER_URL'])
+        win.webContents.openDevTools()
     } else {
         win.loadFile(join(__dirname, '../renderer/index.html'))
     }
