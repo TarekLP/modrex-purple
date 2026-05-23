c408c4a7a11a3c91e5d1291702bb345f8f6a42e0
ShulhaOleh <broo20544@gmail.com>
2026-05-22 23:39:26 -0400

    feat(launchers): add PD3 game definition

---
+++ src/main/launchers/games/index.ts
@@ -0,0 +1,6 @@
+import type { GameDef } from '../types'
+import { PD3 } from './pd3'
+
+export const GAMES: Record<string, GameDef> = {
+    pd3: PD3,
+}
---
+++ src/main/launchers/games/pd3.ts
@@ -0,0 +1,22 @@
+import type { GameDef } from '../types'
+
+export const PD3: GameDef = {
+    id: 'pd3',
+    name: 'PAYDAY 3',
+    executable: 'PAYDAY3.exe',
+    modsPath: 'PAYDAY3/Content/Paks/~mods',
+    modExtensions: ['.pak'],
+    launchers: {
+        steam: {
+            appId: 1272080,
+            folderName: 'PAYDAY3',
+        },
+        epic: {
+            displayName: 'PAYDAY 3',
+            slug: 'payday-3',
+        },
+        xbox: {
+            productId: '9NPZVDCH73SX',
+        },
+    },
+}
