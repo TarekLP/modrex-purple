e910f86ae5e3033307ee45d4dbfb1af5de0295e4
ShulhaOleh <broo20544@gmail.com>
2026-05-23 19:36:29 -0400

    feat(tauri): port settings to rust

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,5 +1,11 @@
+mod commands;
+
 pub fn run() {
-    tauri::Builder::default()
-        .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+    let app = tauri::Builder::default()
+        .build(tauri::generate_context!())
+        .expect("error while building tauri application");
+
+    commands::settings::migrate_from_electron(app.handle());
+
+    app.run(|_, _| {});
 }
